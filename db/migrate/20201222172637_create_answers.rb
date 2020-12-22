29acd5a3dfd5671cd8966927339e7cebcb919e89
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-22 20:38:51 +0300

    2nd task complete
    ## Создал модели:
    * Тест
    * Вопрос
    * Ответ
    * Пользователь
    * Категория
    
    Определенные поля не могут содержать null
    
    default values to tests->level && answers->correct

---
+++ db/migrate/20201222172637_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :title, null: false
+      t.boolean :correct, default: false
+
+      t.timestamps
+    end
+  end
+end
