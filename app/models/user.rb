712fa0e590dd14d114712afbea09708819d39699
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-24 18:13:24 +0300

    final 3rd(not sure)
    * created user_tests table
    * added class method "names_by_category" to Test model and instance method "tests_passed" to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    def tests_passed(level)
+        Test.joins('INNER JOIN user_tests ON user_tests.test_id = tests.id')
+        .where(tests: { level: level }, user_tests: { user_id: id })
+    end
 end
