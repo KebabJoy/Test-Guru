2226d2e0c398b2a3eace0ba3ea19ae6bc21670b7
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-25 19:28:24 +0300

    4th_task final
    added associations to models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,3 @@
 class Answer < ApplicationRecord
+    belongs_to :question, dependent: :destroy
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions, dependent: :destroy
+  has_many :user_tests, dependent: :destroy
+  has_many :users, through: :tests_users
+  
   def self.names_by_category(category)
     joins("INNER JOIN categories ON categories.id = tests.category_id").
     where(categories: { title: category}).order(title: :desc).pluck(:title)
