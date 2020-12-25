61dfb7306f8ed4cec701fd24850f0034590a825f
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-25 21:10:38 +0300

    refact
    added authored tests
    refactored methods in models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author
   has_many :questions, dependent: :destroy
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :tests_users
   
   def self.names_by_category(category)
-    joins("INNER JOIN categories ON categories.id = tests.category_id").
+    joins(:categories).
     where(categories: { title: category}).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_many :user_tests, dependent: :destroy
-    has_many :tests, through: :user_tests, dependent: :destroy
+    has_many :tests, foreign_key: "author_id" ,through: :user_tests, dependent: :destroy
     
     def tests_passed(level)
-        Test.joins('INNER JOIN user_tests ON user_tests.test_id = tests.id')
+        Test.joins(:user_tests)
         .where(tests: { level: level }, user_tests: { user_id: id })
     end
 end
