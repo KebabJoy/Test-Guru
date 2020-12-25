af83c89e9bc000dce26c729f0bf991d1edf24705
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-26 01:47:01 +0300

    refact
    I realised I had to pass author myself, *this language is so random 0_o*

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :questions, dependent: :destroy
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :tests_users
   
   def self.names_by_category(category)
-    joins(:categories).
+    Test.joins(:categories).
     where(categories: { title: category}).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :user_tests, dependent: :destroy
+    has_many :user_tests
     has_many :tests, foreign_key: "author_id" ,through: :user_tests, dependent: :destroy
     
     def tests_passed(level)
