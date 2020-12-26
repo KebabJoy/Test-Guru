d9328994abf1528ad6d5717a36e029c4eb65a846
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-26 17:08:42 +0300

    4th_task final (#4)
    * 4th_task final
    
    added associations to models
    
    * refact
    
    added authored tests
    refactored methods in models
    
    * refact
    
    I realised I had to pass author myself, *this language is so random 0_o*
    
    * revised
    
    changed :categories to :category in test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :questions, dependent: :destroy
+  has_many :user_tests, dependent: :destroy
+  has_many :users, through: :tests_users
+  
   def self.names_by_category(category)
-    joins("INNER JOIN categories ON categories.id = tests.category_id").
+    Test.joins(:category).
     where(categories: { title: category}).order(title: :desc).pluck(:title)
   end
 end
