06c04e7fbc339feba5d9f547018bf9bf8b5d1061
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-26 09:45:27 +0300

    revised
    changed :categories to :category in test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,7 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   
   def self.names_by_category(category)
-    Test.joins(:categories).
+    Test.joins(:category).
     where(categories: { title: category}).order(title: :desc).pluck(:title)
   end
 end
