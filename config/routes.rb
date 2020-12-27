451445cd281237d0f2eaa76d259ed65a1a3815e7
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-12-27 18:48:22 +0300

    starting off

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tests#index'
   
-  get '/tests/:id/start', to: 'tests#start'
-  get '/tests/:id/start', to: 'tasks#start'
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
