3391e295db6904380ab127ea34e2d114691bd07c
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-02-04 20:58:00 +0300

    trying to fix seeds(2)
    Ran out of ideas

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_144423) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
