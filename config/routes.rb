c8204a6b6e488eed0095d099c97786ce4a1621d5
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-02-06 22:43:34 +0300

    fixed showing ..questions/:question_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tests#index'
-  
+  get '/tests/:test_id/questions/:question_id', to: 'questions#show'
+
   resources :tests do
     resources :questions, shallow: true
   end
