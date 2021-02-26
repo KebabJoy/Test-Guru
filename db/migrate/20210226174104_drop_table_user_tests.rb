1a232141ded9610b8029b0b72df28e6e225812ad
Nikita Sidorov <50750550+KebabJoy@users.noreply.github.com>
2021-02-26 20:48:18 +0300

    Drop table User_Tests (#10)
    * reversible migration
    
    * end.

---
+++ db/migrate/20210226174104_drop_table_user_tests.rb
@@ -0,0 +1,10 @@
+class DropTableUserTests < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :user_tests do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :test, null: false, foreign_key: true
+
+      t.timestamps
+    end 
+  end
+end
