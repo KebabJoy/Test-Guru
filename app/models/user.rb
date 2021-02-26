99f6123c8f97cec42aac13da2f1044f0c51edb92
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-02-26 12:00:17 +0300

    spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,20 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-    has_many :authored_tests, class_name: 'Test', foreign_key: "author_id" , dependent: :nullify
-    has_many :test_passages
-    has_many :tests, through: :test_passages
+  has_many :authored_tests, class_name: 'Test', foreign_key: "author_id" , dependent: :nullify
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
 
-    has_secure_password
+  has_secure_password
 
-    def tests_passed(lvl)
-        tests.where(level: lvl)
-    end
+  def tests_passed(lvl)
+    tests.where(level: lvl)
+  end
 
-    def test_passage(test)
-        test_passages.order(id: :desc).find_by(test_id: test.id)
-    end
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
