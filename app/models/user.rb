7e51cf25d00b05c734e9f275722317a20e020056
Nikita Sidorov <50750550+KebabJoy@users.noreply.github.com>
2021-02-26 20:28:53 +0300

    9th task (#9)
    * starting off
    
    * lections
    
    * done
    
    по заданию
    
    * refact
    
    Запретил создавать вопросы и ответы неавторизованным пользователям
    Поменял Regex для почты
    Изменил helper для сессий
    
    * user left
    
    * Fixed
    
    * spaces
    
    * refact
    
    Перенесу droptable в новый ПР

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
-    has_many :user_tests
-    has_many :authored_tests, foreign_key: "author_id" ,through: :user_tests, dependent: :nullify
-    has_many :test_passages
-    has_many :tests, through: :test_passages
 
-    validates :name, presence: true
-                                
-    def tests_passed(lvl)
-        tests.where(level: lvl)
-    end
+  has_many :authored_tests, class_name: 'Test', foreign_key: "author_id" , dependent: :nullify
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
-    def test_passage(test)
-        test_passages.order(id: :desc).find_by(test_id: test.id)
-    end
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
+
+  has_secure_password
+
+  def tests_passed(lvl)
+    tests.where(level: lvl)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
