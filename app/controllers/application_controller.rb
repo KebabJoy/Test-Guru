eb0b912b382232a876d67b962dec29139cc3a9d8
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-02-27 16:47:44 +0300

    name apply

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :add_permitted_parameters, if: :devise_controller?
 
+  def add_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 
   def after_sign_in_path_for(user)
     flash[:notice] = "Hello! #{current_user.first_name} #{current_user.last_name}!"
