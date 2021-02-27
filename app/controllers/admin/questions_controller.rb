5ccf72d73bf70170901a265f36ccdf1397d2c45c
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-02-27 15:29:32 +0300

    a lot of changes
    Добавил админа
    Запретил не админу смотреть вопросы, менять тесты
    Добавил мейлеры

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,24 +1,23 @@
-class QuestionsController < ApplicationController
+class Admin::QuestionsController < Admin::BaseController
 
-  before_action :authenticate_user!
   before_action :set_test, only: %i[create new]
   before_action :set_question, only: %i[show destroy edit update]
-  
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def edit; end
 
-  def show 
+  def show
     @answers = @question.answers
   end
-  
+
   def new
     @question = @test.questions.new
   end
-  
+
   def create
     @question = @test.questions.new(question_params)
-      
+
     if @question.save
       redirect_to @question
     else
@@ -33,18 +32,18 @@ class QuestionsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @question.destroy
     redirect_to test_path(@question.test)
   end
 
   private
-  
+
   def question_params
     params.require(:question).permit(:body)
   end
-  
+
   def set_question
     @question = Question.find(params[:id])
   end
@@ -52,8 +51,8 @@ class QuestionsController < ApplicationController
   def set_test
     @test = Test.find(params[:test_id])
   end
-  
+
   def rescue_with_question_not_found
-    render plain: "Such test was not found" 
+    render plain: "Such test was not found"
   end
 end
