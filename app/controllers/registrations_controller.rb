371db94f082d0bfc80a925041ce856ed1f195f0b
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-03-02 08:48:13 +0300

    delete reg controller

--- app/controllers/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-
-  def create
-    super
-    flash[:notice] = "Hello! #{current_user.first_name} #{current_user.last_name}!"
-  end
-end
