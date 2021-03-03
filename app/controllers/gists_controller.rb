c2f7cfc7c0d5287543b640593b2241c83a4e4db3
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-03-03 16:57:25 +0300

    Now it works fine

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,9 +1,10 @@
 class GistsController < ApplicationController
   def create
     test_passage = TestPassage.find(params[:test_passage_id])
-    result = GistQuestionService.new(test_passage.current_question).call
+    service = GistQuestionService.new(test_passage.current_question)
+    result = service.call
 
-    if result.success?
+    if service.success?
       Gist.create!(user: current_user, question: test_passage.current_question, url: result.html_url)
       flash_options = {notice: "#{ActionController::Base.helpers.link_to(t('.success'), result.html_url,rel: 'nofollow', target: '_blank')}"}
     else
