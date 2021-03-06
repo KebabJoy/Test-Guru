66af4c4df3f10d3c20a331aad7b5ae26c09058a2
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-03-06 16:11:53 +0300

    sorting
    Переделал в ООП стиле

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,37 +1,53 @@
+class SortingTable{
+    constructor(control){
+        this.control = control
+        this.table = document.querySelector('table')
+        this.rows = this.table.querySelectorAll('tr')
+        this.sortedRows = []
+        this.sortedTable = document.createElement('table')
+        this.arrowUp = this.table.querySelector('.octicon-arrow-up')
+        this.arrowDown = this.table.querySelector('.octicon-arrow-down')
+        
+        this.setup()
+    }
+
+    sortRowsByTitle(){
+        if(this.arrowUp.classList.contains('hide')){
+            this.sortedRows = Array.from(this.rows)
+                .slice(1)
+                .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
+            this.arrowDown.classList.add('hide')
+            this.arrowUp.classList.remove('hide')
+        } else{
+            this.sortedRows = Array.from(this.rows)
+                .slice(1)
+                .sort((rowA, rowB) => rowA.cells[0].innerHTML < rowB.cells[0].innerHTML ? 1 : -1);
+            this.arrowDown.classList.remove('hide')
+            this.arrowUp.classList.add('hide')
+        }
+
+        this.sortedTable.classList.add('table')
+        this.sortedTable.appendChild(this.rows[0])
+        for (let i = 0; i < this.sortedRows.length; i++){
+            this.sortedTable.appendChild(this.sortedRows[i])
+        }
+        this.table.parentNode.replaceChild(this.sortedTable, this.table)
+    }
+
+    setup(){
+        this.control.addEventListener('click',event => {
+            this.sortRowsByTitle()
+        })
+    }
+}
+
+
 document.addEventListener('turbolinks:load', function(){
     const control = document.querySelector('.sort-by-title')
 
-    if (control) {control.addEventListener('click', sortRowsByTitle)}
+    if (control) new SortingTable(control)
 })
 
-function sortRowsByTitle(){
-    const table = document.querySelector('table')
 
-    // NodeList
-    const rows = table.querySelectorAll('tr')
-    let sortedRows = []
-    const sortedTable = document.createElement('table')
-    
-    if(this.querySelector('.octicon-arrow-up').classList.contains('hide')){
-        sortedRows = Array.from(table.rows)
-            .slice(1)
-            .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
-        this.querySelector('.octicon-arrow-up').classList.remove('hide')
-        this.querySelector('.octicon-arrow-down').classList.add('hide')
-    } else{
-        sortedRows = Array.from(table.rows)
-            .slice(1)
-            .sort((rowA, rowB) => rowA.cells[0].innerHTML < rowB.cells[0].innerHTML ? 1 : -1);
-        this.querySelector('.octicon-arrow-down').classList.remove('hide')
-        this.querySelector('.octicon-arrow-up').classList.add('hide')
-    }
 
-    sortedTable.classList.add('table')
-    sortedTable.appendChild(rows[0])
 
-    for (let i = 0; i < sortedRows.length; i++){
-        sortedTable.appendChild(sortedRows[i])
-    }
-
-    table.parentNode.replaceChild(sortedTable, table)
-}
